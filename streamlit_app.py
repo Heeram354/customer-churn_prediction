66aff4e241996c773e3aad1f659a2db2ad0bafef
Gauri-Ingale <ingalegauri2002@gmail.com>
2025-11-15 18:33:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1DFjhljPz7onT3DqrRFcTd43hjDhObdnu
 """
 
-!pip install streamlit
 import streamlit as st
 import pandas as pd
 import pickle
